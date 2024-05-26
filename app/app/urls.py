399b557899a80f0a2af8631ceb323daba16496c9
mrrahbarnia <mrrahbarnia@gmail.com>
2024-05-26 15:41:17 +0330

    Started project and added some basic configs.

---
+++ app/app/urls.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # ======= Accounts URL's ======= #
+    path('accounts/', include('accounts.urls')),
+
+    # ========== API schema generator ========== #
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'api/', SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
